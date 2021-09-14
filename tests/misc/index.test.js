6910bc85ac77f43d61f03494a107f3c676281cbe
Evgeniy Dashkevich <jabs.box@gmail.com>
2021-09-14 22:42:31 +0300

    misc: doctype

---
+++ tests/misc/index.test.js
@@ -0,0 +1,16 @@
+const { resolve } = require('path');
+const { readFile } = require('fs/promises');
+const { convert } = require('../../index');
+
+describe('misc / doctype,TBD', () => {
+    test.each([
+        ['doctype'],
+    ])('%s', async (path) => {
+        path = resolve(__dirname, path);
+
+        const actual = convert([`${path}.pug`])[`${path}.pug`];
+        const expected = await readFile(`${path}.jsx`, { encoding: 'utf-8' });
+
+        expect(actual).toBe(expected.replace(/\n$/, ''));
+    });
+});
