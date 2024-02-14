aeb24b302eb6f763099915ce7098cd77da6e261c
Evgeniy Dashkevich <jabs.box@gmail.com>
2024-02-14 23:54:00 +0100

    add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,41 +1,33 @@
 module.exports = {
-    'env': {
-        'node': true,
-        'es2021': true
+    env: {
+        node: true,
+        es2021: true,
     },
-    'extends': ['eslint:recommended', 'plugin:jest/recommended'],
-    'parserOptions': {
-        'ecmaVersion': 12,
-        'sourceType': 'module'
+    extends: ['eslint:recommended', 'plugin:jest/recommended', 'prettier'],
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
     },
-    'plugins': ['jest'],
-    'rules': {
-        'indent': [
+    plugins: ['jest', 'prettier'],
+    rules: {
+        'prettier/prettier': [
             'error',
-            4,
-            { 'SwitchCase': 1 }
+            {
+                tagWidth: 4,
+                singleQuote: true,
+                printWidth: 120,
+            },
         ],
-        'linebreak-style': [
-            'error',
-            'unix'
-        ],
-        'quotes': [
-            'error',
-            'single'
-        ],
-        'semi': [
-            'error',
-            'always'
-        ],
+        'linebreak-style': ['error', 'unix'],
     },
-    'overrides': [
+    overrides: [
         {
-            'files': ['*.jsx'],
-            'parserOptions': {
-                'ecmaFeatures': {
-                    'jsx': true,
-                }
-            }
-        }
-    ]
+            files: ['*.jsx'],
+            parserOptions: {
+                ecmaFeatures: {
+                    jsx: true,
+                },
+            },
+        },
+    ],
 };
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    testEnvironment: "node",
-    testMatch: ["**/?(*.)+(spec|test).[tj]s?(x)"],
+    testEnvironment: 'node',
+    testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
 };
